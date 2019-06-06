b4542c6b3a3daf73dc462ffec623ddd3acf524df
sc30 <maiabe@hawaii.edu>
2019-06-06 02:49:15 -1000

    Landing_Page initial commit
    - Insert background-image
    - Insert title
    - Insert Get started button
    - Responsive Design

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Button } from 'semantic-ui-react';
+import { withRouter, NavLink } from 'react-router-dom';
+import '../stylesheets/landing_stylesheet.css';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div id="landing">
+            <Grid.Row id="grid_rows1">
+                <p>Manage your money to increase your savings.</p>
+            </Grid.Row>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Row id="grid_rows2">
+                <p>Get motivated to achieve your goals.</p>
+            </Grid.Row>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Row id="grid_rows3">
+                <h1>EarnIt.</h1>
+            </Grid.Row>
 
-        </Grid>
+            <Grid.Row id="grid_rows4">
+              <Button id="getstarted_button" size='big' color='blue' as={NavLink} activeClassName="active" exact to="/">
+                Get Started!</Button>
+            </Grid.Row>
+        </div>
     );
   }
 }
