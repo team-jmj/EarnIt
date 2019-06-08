521670ece375e963ea0c9d031aa49a6196373cb1
Julie Chai <juliechai75@gmail.com>
2019-06-07 15:06:58 -1000

    Minor

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -5,15 +5,40 @@ const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class UserHome extends React.Component {
   render() {
-    const options = {
+    const options1 = {
       animationEnabled: true,
-      exportEnabled: true,
       theme: "light1", // "light1", "dark1", "dark2"
       title:{
-        text: "June Expenses"
+        text: "June"
       },
       data: [{
         type: "pie",
+        showInLegend: true,
+        legendText: "{label}",
+        indexLabelPlacement: "outside",
+        indexLabel: "{label}: {y}%",
+        startAngle: -90,
+        dataPoints: [
+          { y: 20, label: "Bills" },
+          { y: 24, label: "Rent" },
+          { y: 20, label: "Food" },
+          { y: 14, label: "School" },
+          { y: 12, label: "Leisure" },
+          { y: 10, label: "Other" }
+        ]
+      }]
+    };
+    const options2 = {
+      animationEnabled: true,
+      theme: "light1", // "light1", "dark1", "dark2"
+      title:{
+        text: "2019"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        legendText: "{label}",
+        indexLabelPlacement: "outside",
         indexLabel: "{label}: {y}%",
         startAngle: -90,
         dataPoints: [
@@ -34,10 +59,17 @@ class UserHome extends React.Component {
         <Container textAlign='center'>
           <h1>{date}</h1>
           <h1>This month you'be saved: $</h1>
-          <Grid>
-            <CanvasJSChart options={options}
-                /* onRef={ref => this.chart = ref} */
-            />
+          <Grid centered container>
+            <Grid.Column width={8}>
+              <CanvasJSChart options={options1}
+                  /* onRef={ref => this.chart = ref} */
+              />
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <CanvasJSChart options={options2}
+                  /* onRef={ref => this.chart = ref} */
+              />
+            </Grid.Column>
           </Grid>
         </Container>
     );
