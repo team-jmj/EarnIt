bbf6dbd306c493bc049216c0ed93bd19dde1c343
sc30 <maiabe@hawaii.edu>
2019-06-14 15:31:12 -1000

    Profile Schema
    - create a profile schema

---
+++ app/imports/api/profile/profile.js
@@ -0,0 +1,25 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Profiles = new Mongo.Collection('Profiles');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const ProfileSchema = new SimpleSchema({
+  user: String,
+  savings: Number,
+  owner: String,
+  income: {
+    'income.$': Object,
+    'income.$.date': Date,
+    'income.$.name': String,
+    'income.$.amount': Number,
+  },
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Profiles.attachSchema(ProfileSchema);
+
+/** Make the collection and schema available to other code. */
+export { Profiles, ProfileSchema };
