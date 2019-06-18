f9882278b453a9119a6685a62728416193353bf8
jokabe <okabej3@hawaii.edu>
2019-06-18 03:56:56 -1000

    Setup schema for user expenses of a specific category

--- app/imports/startup/both/index.js
+++ app/imports/startup/both/index.js
@@ -1,2 +1,3 @@
 import '/imports/api/profile';
-import '/imports/api/expenseCategory'
+import '/imports/api/expenseCategory';
+import '/imports/api/userExpense';
--- app/imports/ui/pages/UserExpenses.jsx
+++ app/imports/ui/pages/UserExpenses.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Divider, Grid, Header, Loader, Segment, Card, Container, Icon, Item } from 'semantic-ui-react';
+import { Divider, Grid, Header, Loader, Segment, Card, Container, Icon, Item, Button } from 'semantic-ui-react';
 import { ExpenseCategory, ExpenseCategorySchema } from '/imports/api/expenseCategory/expenseCategory';
 import { Bert } from 'meteor/themeteorchef:bert';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import HiddenField from 'uniforms-semantic/HiddenField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
 import TextField from 'uniforms-semantic/TextField';
-import SubmitField from 'uniforms-semantic/SubmitField';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -27,7 +26,7 @@ class UserExpenses extends React.Component {
     if (error) {
       Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
     } else {
-      Bert.alert({ type: 'success', message: 'Add succeeded' });
+      Bert.alert({ type: 'success', message: 'Category has been Added!' });
       this.formRef.reset();
     }
   }
@@ -39,17 +38,16 @@ class UserExpenses extends React.Component {
 
     ExpenseCategory.insert({ user,
       category}, this.insertCallback);
-
   }
 
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    return this.props.ready ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
   renderPage() {
     return (
         <div className="center-padding">
-          <Grid container centered>
+          <Grid container centered style={{padding: '0%, 0%, 20%, 0%'}}>
             <Grid.Column>
               <Header as="h2" textAlign="center">
                 <p>Add Category</p>
@@ -57,25 +55,27 @@ class UserExpenses extends React.Component {
               <AutoForm ref={(ref) => { this.formRef = ref; }}
                         schema={ExpenseCategorySchema} onSubmit={this.submit}>
                 <Segment>
-                  <TextField name='category' label='New Category'/>
+                  <TextField name="category" label="New Category"/>
                   <ErrorsField/>
-                  <HiddenField name='user' value={Meteor.user().username}/>
+                  <HiddenField name="user" value={Meteor.user().username}/>
                 </Segment>
-                <SubmitField value='Submit'/>
+                <Container textAlign="center">
+                  <Button basic compact size="large" color="blue">Add Category</Button>
+                </Container>
               </AutoForm>
             </Grid.Column>
           </Grid>
 
           <Divider horizontal>
-            <Header as='h2'>
-              <Icon name='table' />
-              Your Expense Categories
+            <Header as="h2">
+              <Icon name="table" />
+              Your Spending Categories
             </Header>
           </Divider>
 
           <Container>
             <Card.Group itemsPerRow={4}>
-              {this.props.categories.map((category) => <CategoryItem key={category._id} category={category} />)}
+              {this.props.categories.map(category => <CategoryItem key={category._id} category={category} />)}
             </Card.Group>
           </Container>
 
