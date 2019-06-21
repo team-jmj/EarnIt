8c09e77d7fc25ae4ce4a1ed3b222909a0ca58b60
jokabe <okabej3@hawaii.edu>
2019-06-20 16:53:24 -1000

    Additional Features in Expense Page
    - Allow users to remove their spending input for a specific category
    - Allow users to edit their spending input for a specific category

--- app/imports/ui/components/CategoryList.jsx
+++ app/imports/ui/components/CategoryList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Modal, Container, Segment, Table } from 'semantic-ui-react';
+import { Icon, Card, Button, Modal, Container, Segment, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
@@ -16,6 +16,7 @@ class CategoryItem extends React.Component {
   constructor(props) {
     super(props);
     this.submit = this.submit.bind(this);
+    this.remove = this.remove.bind(this);
     this.insertCallback = this.insertCallback.bind(this);
   }
 
@@ -23,8 +24,24 @@ class CategoryItem extends React.Component {
     if (error) {
       Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
     } else {
-      Bert.alert({ type: 'success', message: 'Add Success!' });
+      Bert.alert({ type: 'success', message: 'Spending was added!' });
       this.formRef.reset();
+    }
+  }
+
+  updateAlert(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'info', message: 'Spending was updated!' });
+    }
+  }
+
+  removeAlert(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Remove failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'danger', message: 'Spending was removed!' });
     }
   }
 
@@ -34,6 +51,16 @@ class CategoryItem extends React.Component {
 
     UserExpense.insert({ user,
       category_id, amount_spent, description, date, category_name}, this.insertCallback);
+  }
+
+  update(data, id) {
+    const { amount_spent, description, category_id, date, category_name } = data;
+
+    UserExpense.update({_id: id}, { $set: { amount_spent, description, category_id, date, category_name } }, this.updateAlert);
+  }
+
+  remove(id) {
+    UserExpense.remove({_id: id}, this.removeAlert);
   }
 
   render() {
@@ -74,11 +101,39 @@ class CategoryItem extends React.Component {
                           <Table.HeaderCell>Date</Table.HeaderCell>
                           <Table.HeaderCell>Amount</Table.HeaderCell>
                           <Table.HeaderCell>Description</Table.HeaderCell>
+                          <Table.HeaderCell></Table.HeaderCell>
                         </Table.Row>
                       </Table.Header>
                       <Table.Body>
-                          {UserExpense.find({category_id: this.props.category._id }).fetch().map((item, curr) => <Table.Row><Table.Cell key={curr}>{item.date.toDateString()}</Table.Cell>
-                            <Table.Cell key={curr}>{item.amount_spent}</Table.Cell><Table.Cell key={curr}>{item.description}</Table.Cell></Table.Row>)}
+                          {UserExpense.find({category_id: this.props.category._id }).fetch().map((item, curr) => <Table.Row>
+                            <Table.Cell key={curr}>{item.date.toDateString()}</Table.Cell>
+                            <Table.Cell key={curr}>{item.amount_spent}</Table.Cell>
+                            <Table.Cell key={curr}>{item.description}</Table.Cell>
+                            <Table.Cell key={curr}>
+                              <Modal trigger={<Button basic compact icon color="blue" content="Blue">Edit<Icon name="edit outline"/></Button>} closeIcon>
+                                <Modal.Header style={{ textAlign: 'center', backgroundColor: '#0E6EB8', color: '#ffffff' }}>Editing Spending</Modal.Header>
+                                <Modal.Content>
+                                  <Modal.Description>
+                                    <AutoForm ref={(ref) => { this.formRef = ref; }}
+                                              schema={UserExpenseSchema} onSubmit={(event) => this.update(event, item._id)} model={UserExpense.findOne(item._id)}>
+                                      <Segment>
+                                        <DateField name="date" label="Date of Spending:"/>
+                                        <TextField name="amount_spent" label="Input Amount Spent:"/>
+                                        <TextField name="description" label="Description:"/>
+                                        <ErrorsField/>
+                                        <HiddenField name="user" value={Meteor.user().username}/>
+                                        <HiddenField name="category_id" value={this.props.category._id}/>
+                                        <HiddenField name="category_name" value={this.props.category.category}/>
+                                      </Segment>
+                                      <Container textAlign="center">
+                                        <Button basic compact size="large" color="blue">Edit Expense</Button>
+                                      </Container>
+                                    </AutoForm>
+                                  </Modal.Description>
+                                </Modal.Content>
+                              </Modal>
+                              <Button basic compact icon color="red" content="Red" onClick={() => this.remove(item._id)}>Remove<Icon name="trash alternate outline"/></Button>
+                            </Table.Cell></Table.Row>)}
                       </Table.Body>
                     </Table>
                   </Modal.Description>
